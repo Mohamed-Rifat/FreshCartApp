9b6cb94eb5a8b3e2f7a0da15eab6c8ed987d6d49
Mohamed Rifat <mohamed.rifat100@gmail.com>
2025-02-12 02:03:03 +0200

    Create Pages of Products

---
+++ src/Pages/ProductsHome/ProductsHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImageSlider from '../../Component/ImageSlider/ImageSlider'
+import LatestProduct from '../../Component/LatestProduct/LatestProduct'
+
+export default function ProductsHome() {
+  return (
+   <>
+    <ImageSlider/>
+    <LatestProduct/>
+   </>
+  )
+}
